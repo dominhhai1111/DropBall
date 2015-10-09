efbef318cceb7af0c9bfd7bfe6e55a71419715ac
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-10-09 11:22:10 +0700

    first commit

--- DropBall/ViewController.m
+++ DropBall/ViewController.m
@@ -13,15 +13,49 @@
 @end
 
 @implementation ViewController
-
+{
+    UIImageView *ball;
+    NSTimer* timer;
+    CGFloat ballRadius;
+    CGFloat x, y, vellocityY, accellerateY, maxHeight;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self addBall];
+    [self dropBall];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.edgesForExtendedLayout = UIRectEdgeNone; //toa do bo vien tren
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.1
+                                             target:self
+                                           selector:@selector(dropBall)
+                                           userInfo:nil
+                                            repeats:true];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) addBall
+{
+    CGSize mainViewSize = self.view.bounds.size;
+    ball = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Soccer.png"]];
+    ballRadius = 64.0;
+    x= mainViewSize.width * 0.5;
+    y= ballRadius;
+    
+    vellocityY = 0.0;
+    accellerateY = 5.0;
+    CGFloat statusNavigationBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height + self.navigationController.navigationBar.bounds.size.height ;
+    maxHeight= mainViewSize.height - ballRadius - statusNavigationBarHeight ;
+    ball.center = CGPointMake(x, y);
+    [self.view addSubview:ball];
 }
 
+-(void) dropBall
+{
+    vellocityY+= accellerateY;
+    y += vellocityY;
+    if (y> maxHeight) {
+        vellocityY = -vellocityY*0.8;
+        y= maxHeight;
+    }
+    ball.center = CGPointMake(x, y);
+}
 @end
